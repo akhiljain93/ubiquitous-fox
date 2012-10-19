9c61091af5f91eca0d2fe58f8b83f893d38f04a9
Akhil Jain <akhil.jain93@gmail.com>
2012-10-19 12:39:42 +0530

    bakchodi

--- arch/src/Cache.java
+++ arch/src/Cache.java
@@ -19,5 +19,36 @@ public class Cache
 			return false;
 		}	
 	}
-	//public static void 
+	public class L1
+	{
+		Set[] L1 = new Set[512];
+		public L1()
+		{
+			for (int i=0; i<512; i++)
+				L1[i] = new Set(2, 18);
+		}
+		public boolean searchL1(int Tag, int index)
+		{
+			return L1[index].matchTag(Tag);
+		}
+		public void goToL2(int Tag, int index)
+		{
+			if (!searchL1(Tag, index))
+				
+		}
+	}	
+	public class L2
+	{
+		Set[] L2 = new Set[2048];
+		public L2()
+		{
+			for (int i=0; i<2048; i++)
+				L2[i] = new Set(8, 14);
+		}
+		public boolean searchL2(int Tag, int index)
+		{
+			return L2[index].matchTag(Tag);
+		}
+		
+	}
 }
