f35325f44d58c5230385cab289d43f6ff6eab768
Shivanker Goel <shivanker.goel@gmail.com>
2012-10-19 12:39:35 +0530

    set done

--- arch/src/Cache.java
+++ arch/src/Cache.java
@@ -3,21 +3,38 @@ public class Cache
 	public class Set
 	{
 		int assoc;
-		int[] arr;
-		int tagLength;
-		public Set(int assoc, int tag)
+		Integer[] arr;
+		boolean fifo;
+		
+		public Set(int assoc, boolean fifo)
 		{
 			this.assoc = assoc;
-			arr = new int[assoc];
-			tagLength = tag;
+			arr = new Integer[assoc];
+			this.fifo = fifo;
 		}
-		public boolean matchTag(int Tag)
+		
+		public boolean matchTag(int tag)
 		{
 			for (int i=0; i<assoc; i++)
-				if (arr[i] % (1<<assoc) == Tag)
+				if (arr[i] != null && (arr[i] >> 1) == tag)	{
+					if(!fifo)	{
+						Integer a = arr[i];
+						for(int j = i-1; j >= 0; --j)
+							arr[j+1] = arr[j];
+						arr[0] = a;
+					}
 					return true;
+				}
 			return false;
-		}	
+		}
+		
+		public boolean replace(int tag)	{
+			if(matchTag(tag))
+				return false;
+			for(int j = assoc-2; j >= 0; --j)
+				arr[j+1] = arr[j];
+			arr[0] = tag << 1;
+			return true;
+		}
 	}
-	//public static void 
 }
