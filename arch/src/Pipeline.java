63551a797bb2c45c52fedc21a6d43d8a6d359e18
Shivanker Goel <shivanker.goel@gmail.com>
2012-10-24 15:55:54 +0530

    bad stats

--- arch/src/Pipeline.java
+++ arch/src/Pipeline.java
@@ -21,8 +21,6 @@ public class Pipeline {
 		int clock = 0; // no. of clock cycles taken
 		int bubbles = 0; // maintain a bubble counter
 
-		// TODO
-		// array of latches in the pipeline..(To form a circular queue or something later) pipeline[0] is for IF [1] ID [2] for ALU [3]for MEM [4] for WB
 		Instructions[] pipe = new Instructions[5];
 
 		BranchPrediction predictor = new BranchPrediction();
@@ -32,13 +30,16 @@ public class Pipeline {
 		BufferedReader ex = new BufferedReader(new FileReader("exec_trace.txt")); // for execution trace
 		BufferedReader in = new BufferedReader(new FileReader("inst_trace.txt")); // for instruction file
 
-		final int TABLE_SIZE = 100000007;
+		final int TABLE_SIZE = 10000019;
 		// hash table to store all instructions in the instruction file
 		LinkedList<Instructions> hashTable[] = (LinkedList<Instructions>[]) new LinkedList[TABLE_SIZE];
 		
 		// preprocessing of the inst file
 		for (int i = 0; i < TABLE_SIZE; i++)
 			hashTable[i] = new LinkedList<Instructions>();
+		
+		System.err.println("Array initialised!");
+		
 		String s;
 		while ((s = in.readLine()) != null) {
 			StringTokenizer st = new StringTokenizer(s);
@@ -53,16 +54,17 @@ public class Pipeline {
 		}
 		in.close();
 
+		System.err.println("Instructions hashed!");
+		
 		long last = 0, dinst = 0;
 		while (last < 4) {
 			// maintain a loop running through the entire ins file
 			if (last > 0)
 				last++;
-			dinst++;
 			Instructions latest = null;
 
 			if (bubbles != 0)
-				; // if bubbles !=0 insert bubble don't read
+				bubbles--; // if bubbles !=0 insert bubble don't read
 
 			else if (dont_read) // set when a load-use hazard occured in the previous cycle
 				latest = pipe[0];
@@ -72,7 +74,8 @@ public class Pipeline {
 				String line = ex.readLine();
 				if (line == null && last == 0)
 					last++;
-				else {
+				else if (line != null) {
+					dinst++;
 					StringTokenizer st = new StringTokenizer(line);
 					long pc = Long.parseLong(st.nextToken().substring(2), 16);
 					int key = (int) (pc % TABLE_SIZE);
@@ -102,8 +105,9 @@ public class Pipeline {
 			// Load-Use hazard : stall if same register, except when the new
 			// instr. itself is load/store
 			if (pipe[0] != null && pipe[1] != null && pipe[1].type == 0) {
-				if ((pipe[0].type == 2 && (pipe[0].rs == pipe[1].rd || pipe[0].rt == pipe[1].rd)) // alu
-						|| (pipe[0].type == 3 && pipe[0].rs == pipe[1].rd)) // branch
+				if (((pipe[0].type & 1) == 0 && (pipe[0].rs == pipe[1].rd || pipe[0].rt == pipe[1].rd)) // alu and load
+						|| (pipe[0].type == 3 && pipe[0].rs == pipe[1].rd) // branch
+						|| (pipe[0].type == 1 && (pipe[0].rs == pipe[1].rd || pipe[0].rd == pipe[1].rd) )) // store
 					dont_read = true;
 			}
 			// in IF stage, if ins is branch ..ask for branch prediction
@@ -121,6 +125,9 @@ public class Pipeline {
 				// ? Load : Store
 				clock += pipe[3].type == 0 ? cache.access(pipe[3].mem, false) : cache.access(pipe[3].mem, true);
 			else clock++; // cycle anyways complete
+
+			if(dinst % 100000 == 0)
+				System.err.println("No. of dynamic instructions processed: " + dinst + "; last: " + last);
 		}
 
 		ex.close();
