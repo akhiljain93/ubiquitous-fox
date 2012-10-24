5a92a60bcc592b5452dd2195ce66d40cd6b2531d
Shivanker Goel <shivanker.goel@gmail.com>
2012-10-24 12:00:54 +0530

    things

--- arch/src/Pipeline.java
+++ arch/src/Pipeline.java
@@ -6,8 +6,9 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 class Instructions {
-	int pc, type, rs, rt, rd, mem;
-	boolean branch; // outcome of branch
+
+	int		pc, type, rs, rt, rd, mem;
+	boolean	branch;					// outcome of branch
 
 	Instructions() {
 		branch = true;
@@ -18,43 +19,27 @@ public class Pipeline {
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
+
 		int clock = 0; // no. of clock cycles taken
-		//eclipse warning de raha hai, check it out TODO
 		int bubbles = 0; // maintain a bubble counter
-		Instructions[] pipe = new Instructions[5]; // array of latches in the
-													// pipeline..(To form a
-													// circular queue or
-													// something later) TODO
-													// pipeline[0] is for IF [1]
-													// for ID [2] for ALU [3]for
-													// MEM [4] for WB
+
+		// TODO
+		// array of latches in the pipeline..(To form a circular queue or something later) pipeline[0] is for IF [1] ID [2] for ALU [3]for MEM [4] for WB
+		Instructions[] pipe = new Instructions[5];
 
 		BranchPrediction predictor = new BranchPrediction();
 		Cache cache = new Cache();
 		boolean dont_read = false;
 
-		BufferedReader ex = new BufferedReader(new FileReader("exec_trace.txt")); // for
-																					// execution
-																					// trace
-		BufferedReader in = new BufferedReader(new FileReader("inst_trace.txt")); // for
-																					// instruction
-																					// file
-		final int TABLE_SIZE = Integer.MAX_VALUE / 16; // MAX_VALUE/16 was the
-														// best my machine could
-														// support, anyone knows
-														// how to increase the
-														// length of the array??
-														// TODO
-		LinkedList<Instructions> hashTable[] = (LinkedList<Instructions>[]) new LinkedList[TABLE_SIZE]; // hash
-																										// table
-																										// to
-																										// store
-																										// all
-																										// instructions
-																										// in
-																										// the
-																										// instruction
-																										// file
+		BufferedReader ex = new BufferedReader(new FileReader("exec_trace.txt")); // for execution trace
+		BufferedReader in = new BufferedReader(new FileReader("inst_trace.txt")); // for instruction file
+
+		// TODO
+		// MAX_VALUE/16 was the best my machine could support, anyone knows how to increase the length of the array??
+		final int TABLE_SIZE = Integer.MAX_VALUE / 16;
+		// hash table to store all instructions in the instruction file
+		LinkedList<Instructions> hashTable[] = (LinkedList<Instructions>[]) new LinkedList[TABLE_SIZE];
+
 		// preprocessing of the inst file
 		for (int i = 0; i < TABLE_SIZE; i++)
 			hashTable[i] = new LinkedList<Instructions>();
@@ -73,40 +58,42 @@ public class Pipeline {
 			hashTable[key].add(ins);
 		}
 		in.close();
-		while (true) {//check termination condition TODO dekh lo kab terminate karna hai loop ko
+
+		int last = 0, dinst = 0;
+		while (last < 4) {			
 			// maintain a loop running through the entire ins file
+			if(last > 0)
+				last++;
+			dinst++;
 			Instructions latest = null;
 
 			if (bubbles != 0)
-				; // if bubbles !=0 insert bubble
-			// don't read
+				; // if bubbles !=0 insert bubble don't read
 
-			else if (dont_read) // set when a load-use hazard occured in the
-								// previous cycle
+			else if (dont_read) // set when a load-use hazard occured in the previous cycle
 				latest = pipe[0];
 
 			// reading the file only when bubbles == 0 && dont_read == false
 			else {
-				StringTokenizer st = new StringTokenizer(ex.readLine());
-				int pc = (int) Long.parseLong(st.nextToken(), 16);
-				int key = (pc % TABLE_SIZE + TABLE_SIZE) % TABLE_SIZE;
-				Iterator<Instructions> i = hashTable[key].iterator();
-				while (i.hasNext()) {
-					Instructions next = i.next();
-					if (next.pc == pc)
-						latest = next;
+				String line = ex.readLine();
+				if (line == null && last == 0)
+					last++;
+				else {
+					StringTokenizer st = new StringTokenizer(line);
+					int pc = (int) Long.parseLong(st.nextToken().substring(2), 16);
+
+					// TODO why is this needed? isn't pc always positive?
+					int key = (pc % TABLE_SIZE + TABLE_SIZE) % TABLE_SIZE;
+					Iterator<Instructions> i = hashTable[key].iterator();
+					while (i.hasNext()) {
+						Instructions next = i.next();
+						if (next.pc == pc)
+							latest = next;
+					}
+					latest.mem = (int) Long.parseLong(st.nextToken().substring(2), 16); // mem from trace file
+					latest.branch = (Integer.parseInt(st.nextToken()) == 0) ? false : true;// branch outcome from trace file...true for taken
 				}
-				latest.mem = (int) Long.parseLong(st.nextToken(), 16); // mem
-																		// from
-																		// trace
-																		// file
-				int result = Integer.parseInt(st.nextToken());
-				latest.branch = (result == 0) ? false : true;// branch outcome
-																// from trace
-																// file...true
-																// for taken
 			}
-			ex.close();
 			// move the pipeline 1 step forward
 			for (int j = 4; j > 0; j--)
 				pipe[j] = pipe[j - 1];
@@ -127,8 +114,7 @@ public class Pipeline {
 			}
 			// in IF stage, if ins is branch ..ask for branch prediction
 			if (!dont_read && pipe[0] != null && pipe[0].type == 3)
-				if (pipe[0].branch != predictor.predict(pipe[0].pc,
-						pipe[0].branch))
+				if (pipe[0].branch != predictor.predict(pipe[0].pc, pipe[0].branch))
 					bubbles += 2;
 
 			/******* ALU *******/
@@ -139,11 +125,12 @@ public class Pipeline {
 			/******* MEM *******/
 			if (pipe[3] != null && pipe[3].type < 2)
 				// ? Load : Store
-				clock += pipe[3].type == 0 ? cache.access(pipe[3].mem, false)
-						: cache.access(pipe[3].mem, true);
-			else
-				clock++; // cycle anyways complete
+				clock += pipe[3].type == 0 ? cache.access(pipe[3].mem, false) : cache.access(pipe[3].mem, true);
+			else clock++; // cycle anyways complete
 		}
+
+		ex.close();
+		System.out.println((dinst/clock)+" ");
 
 	}
 }
