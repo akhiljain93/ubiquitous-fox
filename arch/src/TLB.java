2a142e4c43660e9ed4f39c0c8d9ba02184c5d39b
Akhil Jain <akhil.jain93@gmail.com>
2012-11-08 16:56:44 +0530

    write implemented

--- arch/src/TLB.java
+++ arch/src/TLB.java
@@ -1,21 +1,68 @@
+import java.util.LinkedList;
 
-public class TLB
-{
-	public class PageTableLevel
-	{
-		long va[] = new long[1024];
-		long pa[] = new long[1024];
-		boolean dirty[] = new boolean[1024];
-		boolean valid[] = new boolean[1024];
-		boolean ref[] = new boolean[1024];
-		PageTableLevel next[] = new PageTableLevel[1024];
+public class TLB {
+	final int size = 1024;
+
+	public class Row {
+		long va, pa;
+		boolean dirty, ref;
+
+		Row() {
+			va = pa = 0;
+			dirty = ref = false;
+		}
 	}
-	long va[] = new long[1024];
-	long pa[] = new long[1024];
-	boolean dirty[] = new boolean[1024];
-	boolean valid[] = new boolean[1024];
-	public void writeToTLB(long add)
-	{
-		
+
+	public class PageTableLevel2 {
+		Row table[] = new Row[size];
+
+		public PageTableLevel2() {
+			for (int i = 0; i < size; i++)
+				table[i] = new Row();
+		}
+	}
+
+	PageTableLevel2 pt[] = new PageTableLevel2[size];
+
+	Row Buffer[] = new Row[size];
+	LinkedList<Row> LRUCalculator = new LinkedList<Row>();
+
+	TLB() {
+		for (int i = 0; i < size; i++) {
+			Buffer[i] = new Row();
+			pt[i] = new PageTableLevel2();
+		}
+	}
+
+	public int writeToTLB(long add) {
+		for (int i = 0; i < size; i++)
+			if (add == Buffer[i].va) {
+				System.out.println("TLB Hit");
+				LRUCalculator.addFirst(LRUCalculator.remove(LRUCalculator
+						.indexOf(Buffer[i])));
+				return 0;
+			}
+		System.out.println("TLB Miss");
+		int pt1 = (int) (add & 0xFFC00000) >>> 22;
+		int pt2 = (int) (add & 0x3FF000) >>> 12;
+		if (pt[pt1] == null) { // serves purpose of valid bit
+			int hardDiskAccessTime = 0;
+			System.out.println("Page Fault!!");
+			return hardDiskAccessTime;
+		}
+		System.out.println("Page Table Hit");
+		Row found = pt[pt1].table[pt2];
+		found.dirty = true;
+		if (LRUCalculator.size() == size) {
+			Row del = LRUCalculator.removeLast();
+			for (int i = 0; i < size; i++)
+				if (del == Buffer[i]) {
+					Buffer[i] = found;
+					break;
+				}
+		}
+		LRUCalculator.addFirst(found);
+		found.ref = true;
+		return 30;
 	}
 }
